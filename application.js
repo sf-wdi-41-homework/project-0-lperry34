fa4038a439589bdcb53624b134dd4e2d0ea82591
lperry34 <leviperry34@gmail.com>
2017-10-06 12:35:10 -0700

    added a key listener

--- application.js
+++ application.js
@@ -1,18 +1,30 @@
 $(document).ready(function() {
-  var winPoint = 12;
-  var players = [
-    {
-      id: 1,
-      position: 0,
-      keycode: 39,
-      css_locator: '#player1'
-    },
-    {
-      id: 2,
-      position: 0,
-      keycode: 68,
-      css_locator: '#player2'
-    }
-  ];
+  console.log("working");
+    var winPoint = 12;
+    var players = [{
+        position: 0,
+        keycode: 39,
+        css_locator: '#player1'
+    }, {
+        position: 0,
+        keycode: 68,
+        css_locator: '#player2'
+    }];
 
+
+$(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 39) {
+     $("#box1").css("background-image","url(yoshiDude.png)");
+     players[0].position++;
+     console.log(players[0].position);
+   }
+   if(players[0].position === 2){
+    $("#box2").css("background-image","url(yoshiDude.png)");
+    $("#box1").css("background-image","url()");
 }
+
+
+
+  });
+
+});

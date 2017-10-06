0c81fb4525213aacd9b2670e1b56424a4459ea92
lperry34 <leviperry34@gmail.com>
2017-10-06 14:54:57 -0700

    added comments

--- application.js
+++ application.js
@@ -1,47 +1,48 @@
 $(document).ready(function() {
-  console.log("working");
-      var players = [{
+    console.log("working");
+    var players = [{
         position: 1,
     }, {
         position: 1,
     }];
 
-// player 1
+    // player 1
 
- function movePlayer(position) {
-     $(".active").removeClass('active').next().addClass('active');
-     players[0].position++;
-     console.log(players[0].position);
- }
+    function movePlayer(position) {
+        //adds CSS to the class active
+        $(".active").removeClass('active').next().addClass('active') //adds the class to the next element
+        players[0].position++; // increments the position
+        console.log(players[0].position);
+    }
 
- $(document).keydown(function(keyPressed) {
-     if (keyPressed.keyCode == 39) {
-         movePlayer();
-     }
+    $(document).keydown(function(keyPressed) { // runs the function everytime the key is pressed
+        if (keyPressed.keyCode == 39) {
+            movePlayer();
+        }
 
-     if (players[0].position === 9) {
-         alert("player 1 has won!")
-     }
+        if (players[0].position === 9) { //takes incremented position and creates win statement
+            alert("player 1 has won!")
+        }
 
- });
+    });
 
 
-// player 2
+    // player 2
 
-function movePlayer1(position) {
-     $(".active1").removeClass('active1').next().addClass('active1');
-     players[1].position++;
-     console.log(players[1].position);
- }
- $(document).keydown(function(keyPressed) {
-     if (keyPressed.keyCode == 68) {
-         movePlayer1();
-     }
+    function movePlayer1(position) {
+        $(".active1").removeClass('active1').next().addClass('active1');
+        players[1].position++;
+        console.log(players[1].position);
+    }
+    $(document).keydown(function(keyPressed) {
+        if (keyPressed.keyCode == 68) {
+            movePlayer1();
+        }                                                   // all the same thing for player 2
 
-     if (players[1].position === 9) {
-         alert("player 2 has won!")
-     }
+        if (players[1].position === 9) {
+            alert("player 2 has won!")
+        }
 
- });
+    });
 
 });

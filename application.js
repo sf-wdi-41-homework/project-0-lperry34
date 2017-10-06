c36f6109bf7a1bd2faf4ad5959f0888b89139eb5
lperry34 <leviperry34@gmail.com>
2017-10-06 14:42:11 -0700

    made the code dry

--- application.js
+++ application.js
@@ -1,30 +1,33 @@
 $(document).ready(function() {
   console.log("working");
-    var winPoint = 12;
-    var players = [{
-        position: 0,
-        keycode: 39,
-        css_locator: '#player1'
+      var players = [{
+        position: 1,
     }, {
-        position: 0,
-        keycode: 68,
-        css_locator: '#player2'
+        position: 1,
     }];
 
+// player 1
 
-$(document).keydown(function(keyPressed) {
-   if (keyPressed.keyCode == 39) {
-     $("#box1").css("background-image","url(yoshiDude.png)");
+ function movePlayer(position) {
+     $(".active").removeClass('active').next().addClass('active');
      players[0].position++;
      console.log(players[0].position);
-   }
-   if(players[0].position === 2){
-    $("#box2").css("background-image","url(yoshiDude.png)");
-    $("#box1").css("background-image","url()");
-}
+ }
+
+ $(document).keydown(function(keyPressed) {
+     if (keyPressed.keyCode == 39) {
+         movePlayer();
+     }
+
+     if (players[0].position === 9) {
+         alert("player 1 has won!")
+     }
+
+ });
 
 
+// player 2
 
-  });
+
 
 });

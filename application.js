3d2d41028e3226bb764ec5c735fff30d44495e04
lperry34 <leviperry34@gmail.com>
2017-10-06 09:52:23 -0700

    HTML is done. I have a board, and one object in the JS file. I didn't know we were supposed to use the link in slack for the project

---
+++ application.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  var winPoint = 12;
+  var players = [
+    {
+      id: 1,
+      position: 0,
+      keycode: 39,
+      css_locator: '#player1'
+    },
+    {
+      id: 2,
+      position: 0,
+      keycode: 68,
+      css_locator: '#player2'
+    }
+  ];
+
+}

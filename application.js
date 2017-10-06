87669f3eba713fc85d8afb8c735981e1a9011442
lperry34 <leviperry34@gmail.com>
2017-10-06 15:06:41 -0700

    added score tab

--- application.js
+++ application.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
 
         if (players[0].position === 9) { //takes incremented position and creates win statement
             alert("player 1 has won!")
+            $('#1score').append("1");
         }
 
     });
@@ -37,12 +38,17 @@ $(document).ready(function() {
     $(document).keydown(function(keyPressed) {
         if (keyPressed.keyCode == 68) {
             movePlayer1();
-        }                                                   // all the same thing for player 2
+        }                                                // all the same thing for player 2
 
         if (players[1].position === 9) {
-            alert("player 2 has won!")
+            alert("player 2 has won!");
+            $('#score').append("1");
         }
 
     });
+
+
+
+
 
 });
